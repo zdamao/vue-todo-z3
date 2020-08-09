9462bdfadc63f56cf27ce9c849b8fe1bf0575faa
zdamao <zdamao@126.com>
2020-08-09 09:54:15 +0800

    achive fetch

--- src/App.vue
+++ src/App.vue
@@ -20,44 +20,95 @@ export default {
   },
   data() {
     return {
-      employees: [
-        {
-          id: 1,
-          name: "zdamao",
-          email: "zdamao@126.com",
-        },
-        {
-          id: 2,
-          name: "adamao",
-          email: "adamao@126.com",
-        },
-        {
-          id: 3,
-          name: "bdamao",
-          email: "bdamao@126.com",
-        },
-      ],
+      employees: [],
     };
   },
   methods: {
-    addEmployee: function (employee) {
+    //get
+    getEmployees: async function() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        this.employees = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    //post
+    addEmployee: async function(employee) {
       // console.log(employee);
-      const lastId = this.employees[this.employees.length - 1].id;
-      const id = lastId + 1;
-      const newEmployee = { ...employee, id };
-      this.employees = [...this.employees, newEmployee];
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users",
+          {
+            method: "POST",
+            body: JSON.stringify(employee),
+            headers: { "Content-Type": "application/json; charset=UTF-8" },
+          }
+        );
+        const data = await response.json();
+        this.employees = [...this.employees, data];
+      } catch (error) {
+        console.log(error);
+      }
     },
-    deleteEmployee: function (id) {
-      this.employees = this.employees.filter((employee) => {
-        return employee.id !== id;
-      });
+    // addEmployee: function(employee) {
+    //   // console.log(employee);
+    //   const lastId = this.employees[this.employees.length - 1].id;
+    //   const id = lastId + 1;
+    //   const newEmployee = { ...employee, id };
+    //   this.employees = [...this.employees, newEmployee];
+    // },
+
+    // put -> but has 500 error, even cp, i dont konw why
+    editEmployee: async function(id, updatedEmployee) {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${id}`,
+          {
+            method: "PUT",
+            body: JSON.stringify(updatedEmployee),
+            headers: { "Content-Type": "application/json charset=UTF-8" },
+          }
+        );
+        const data = await response.json();
+        this.employees = this.employees.map((employee) => {
+          return employee.id === id ? data : employee;
+        });
+      } catch (error) {
+        console.log(error);
+      }
     },
-    editEmployee: function (id, updatedEmployee) {
-      console.log(id, updatedEmployee);
-      this.employees = this.employees.map((employee) => {
-        return employee.id === id ? updatedEmployee : employee;
-      });
+    // editEmployee: function(id, updatedEmployee) {
+    //   console.log(id, updatedEmployee);
+    //   this.employees = this.employees.map((employee) => {
+    //     return employee.id === id ? updatedEmployee : employee;
+    //   });
+    // },
+
+    //delete
+    deleteEmployee: async function(id) {
+      try {
+        await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+          method: "DELETE",
+        });
+        this.employees = this.employees.filter((employee) => {
+          return employee.id !== id;
+        });
+      } catch (error) {
+        console.log(error);
+      }
     },
+    // deleteEmployee: function(id) {
+    //   this.employees = this.employees.filter((employee) => {
+    //     return employee.id !== id;
+    //   });
+    // },
+  },
+  mounted() {
+    this.getEmployees();
   },
 };
 </script>
